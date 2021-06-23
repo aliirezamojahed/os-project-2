d8d004d781de8dd69797f7739a5ddd01bdb70ff3
Mohammad <dscorpion78@gmail.com>
2021-06-22 18:42:03 -0700

    add built-in commands

--- shell.c
+++ shell.c
@@ -22,31 +22,66 @@ void get_input(char *in)
 	in[2] = NULL;
 }
 
-int main()
-{
-	char *args[3];
-	pid_t pid;
-	
-	get_input(*args);
+void hello() {
+	printf("Hello, Welcome, nice to meet you!\n");
+	// another features...
+}
 
-	while (1) 
-	{
-		pid = fork();	
-		if (pid < 0)
-		{
-			// failure 
-			printf("error!");
-		} 
-		else if (pid == 0)
-		{
-			// pid process
-			execvp(args[0], args);
-		}
-	       	else if (pid > 0)
-		{
-			// parent process
-			wait(NULL);
-		}	
+int changeDirectory(char *path) {
+	// char s[100];
+
+	// printing current working directory
+    // printf("%s\n", getcwd(s, 100));
+	if (chdir(path) != 0) {
+		perror("chdir() is failed!");
 	}
+	// printing current working directory
+    // printf("%s\n", getcwd(s, 100));
+
+	return 0;
+}
+
+int main() {
+	
+	while (1) {
+		
+		char *args[3] = {"byeShell", "..", "NULL"};
+		pid_t pid;
+		// get_input(*args);
+		
+		if (args[0] != "byeShell") {
+			if (args[0] == "helloShell") {
+				hello();
+				// break; // for debug
+			} else if ( args[0] == "cd") {
+				// cd os992 : args[0] = cd, args[1] = path (e.g, os992)
+				changeDirectory(args[1]); // passing path to the function
+				// break; // for debug
+			} else {
+				// TODO e,g. fork
+				pid = fork();	
+				if (pid < 0)
+				{
+					// failure 
+					printf("error!");
+				} 
+				else if (pid == 0)
+				{
+					// pid process
+					execvp(args[0], args);
+				}
+				else if (pid > 0)
+				{
+					// parent process
+					wait(NULL);
+				}
+			}
+		} else {
+			// exit from loop
+			printf("See you later! bye...\n");
+			break;
+		}
+	}	
+	
 	return 0;
 }
