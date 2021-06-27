6007f028aefbb7823f82584912d42f2dc2532721
alireza-m8179 <54576546+alireza-m8179@users.noreply.github.com>
2021-06-27 21:36:59 +0430

    changedir updated for ~ addressing

--- shell.c
+++ shell.c
@@ -48,6 +48,10 @@ int changeDirectory(char *path)
 {
 	char s[100];
 
+	if (strcmp(args[1], "~") == 0)
+	{
+		args[1] = getenv(“HOME”);
+	}
 	if (chdir(path) != 0)
 	{
 		perror("chdir() is failed!");
@@ -96,10 +100,6 @@ int main()
 			} 
 			else if (strcmp(args[0], "cd") == 0) 
 			{
-				if (strcmp(args[1], "~") == 0)
-				{
-					args[1] = HOME;
-				}
 				// cd os992 : args[0] = cd, args[1] = path (e.g, os992)
 				changeDirectory(args[1]); // passing path to the function
 			} 
