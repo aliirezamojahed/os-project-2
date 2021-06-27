ab78e9fff18b3b5c1ef130c00402f6e2759cabd1
alireza_m8179 <dl7.blogfa@gmail.com>
2021-06-27 19:35:32 +0430

    add ~ for cd, variable declaration moved out of loop.

--- shell.c
+++ shell.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-// text colors
+// Text colors
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_BLUE    "\x1b[34m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-
+// Constants
 #define BUFFER_SIZE 64 
 #define ARGS_SIZE 16
 
@@ -21,7 +23,7 @@ int getInput(char *str)
 
 	char *buffer;
 	buffer = readline(ANSI_COLOR_GREEN "FarabiShell>>> " ANSI_COLOR_RESET);
-	// printf("buffer: %s\n", buffer); 
+
 	if (strlen(buffer) != 0)
 	{
 		add_history(buffer);
@@ -30,11 +32,11 @@ int getInput(char *str)
 	} 
 	else
 	{
-	return 1;
+		return 1;
 	}
 }
 
-// helloShell built-in command
+// helloUser built-in command
 void hello() 
 {
 	printf(ANSI_COLOR_CYAN "Hello, welcome, nice to meet you!\n" ANSI_COLOR_RESET);
@@ -46,12 +48,12 @@ int changeDirectory(char *path)
 {
 	char s[100];
 
-	if (chdir(path) != 0) {
+	if (chdir(path) != 0)
+	{
 		perror("chdir() is failed!");
 	}
-	
 	// printing current working directory
-    printf(ANSI_COLOR_BLUE "%s\n" ANSI_COLOR_RESET, getcwd(s, 100));
+  printf(ANSI_COLOR_BLUE "%s\n" ANSI_COLOR_RESET, getcwd(s, 100));
 
 	return 0;
 }
@@ -59,18 +61,25 @@ int changeDirectory(char *path)
 int main() 
 {	
 	printf("\033[H\033[J");  // clear screen
-	char *clear[] = {"clear", NULL};
 	hello();	
 
-	while (1) {
+	pid_t pid;
+	char *args[ARGS_SIZE];
+	char str[BUFFER_SIZE];
+	char *token;
+	int counter;
+	char *HOME = getenv("HOME");
 
-		pid_t pid;
-		char *args[ARGS_SIZE];
-		char str[BUFFER_SIZE];
-		getInput(str);
+	while (1)
+	{
+		if (getInput(str))
+		{
+			printf("input error!");
+			continue;
+		}
 		
-		char *token = strtok(str, " ");
-		int counter = 0;
+		token = strtok(str, " ");
+		counter = 0;
 		
 		while (token != NULL)
 		{
@@ -79,35 +88,37 @@ int main()
 		}
 		args[counter] = NULL;
 				
-		if (strcmp(args[0],"byeShell") != 0) 
+		if (strcmp(args[0], "byeShell") != 0) 
 		{
-			if (strcmp(args[0],"helloShell") == 0) 
+			if (strcmp(args[0], "helloUser") == 0) 
 			{
 				hello();
 			} 
-			else if (strcmp(args[0],"cd") == 0) 
+			else if (strcmp(args[0], "cd") == 0) 
 			{
+				if (strcmp(args[1], "~") == 0)
+				{
+					args[1] = HOME;
+				}
 				// cd os992 : args[0] = cd, args[1] = path (e.g, os992)
 				changeDirectory(args[1]); // passing path to the function
 			} 
 			else 
 			{
-				// TODO e,g. fork
-				
 				pid = fork();	
 				if (pid < 0)
 				{
-					// failure 
-					printf("error!");
+					// Failure 
+					perror("error!");
 				} 
 				else if (pid == 0)
 				{
-					// pid process
+					// Child process
 					execvp(args[0], args);
 				}
 				else if (pid > 0)
 				{
-					// parent process
+					// Parent process
 					wait(NULL);
 				}
 			}
